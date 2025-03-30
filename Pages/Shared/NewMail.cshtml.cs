36437a50eb1a1e020e28da9c90e17fc69f035d36
crmindira007 <crmsystem0007@gmail.com>
2025-03-31 01:03:45 +0530

    change

---
+++ Pages/Shared/NewMail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRM.Pages.Shared
+{
+    public class NewMailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
